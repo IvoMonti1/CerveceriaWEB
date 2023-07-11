94db601301facdd2b690699edae130e9edbc11fe
Ivo Monti <ivomonti1gmail.com>
2023-07-11 15:36:32 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+/*let menu = document.querySelector('.fa-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.addEventListener('click',function(){
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('nav-toggle');
+});
+
+window.addEventListener('scroll', () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('nav-toggle');
+});
+
+*/
+
+function iniciarMap(){
+    var coord = {lat:-34.5844215 ,lng: -60.9423707};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
